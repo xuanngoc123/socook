86e4e402b57dabd5d5980b63cde7c0c19078fb3e
unknown <xuanngochq2k@gmail.com>
2022-04-28 21:20:07 +0700

    fix H12 heroku

--- src/config/config.js
+++ src/config/config.js
@@ -43,6 +43,12 @@ const env = {
         "database": "recipedb",
         "host": process.env.MYSQL_HOST,
         "dialect": "mysql",
+        "pool": {
+            max: 20000,
+            min: 0,
+            acquire: 60000,
+            idle: 10000
+        },
         "define": {
             "freezeTableName": true,
             "createdAt": false,
